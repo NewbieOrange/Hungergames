e56eae57066deebad55074626d4f4d31ab81a0ac
NewibieOrange <yutiancheng1999@gmail.com>
2014-04-07 13:02:34 +0800

    Tell console if it cannot use a command

--- src/me/minebuilders/hg/commands/BaseCmd.java
+++ src/me/minebuilders/hg/commands/BaseCmd.java
@@ -8,41 +8,54 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public abstract class BaseCmd {
+public abstract class BaseCmd
+{
 
-	public CommandSender sender;
-	public String[] args;
-	public String cmdName;
-	public int argLength = 0;
-	public boolean forcePlayer = true;
-	public boolean forceInGame = false;
-	public boolean forceInRegion = false;
-	public String usage = "";
-	public Player player;
+    public CommandSender sender;
+    public String[] args;
+    public String cmdName;
+    public int argLength = 0;
+    public boolean forcePlayer = true;
+    public boolean forceInGame = false;
+    public boolean forceInRegion = false;
+    public String usage = "";
+    public Player player;
 
-	public boolean processCmd(HG p, CommandSender s, String[] arg) {
-		sender = s;
-		args = arg;
+    public boolean processCmd(HG p, CommandSender s, String[] arg)
+    {
+        sender = s;
+        args = arg;
 
-		if (forcePlayer) {
-			if (!(s instanceof Player)) return false;
-			else player = (Player) s;
-		}
-		if (!s.hasPermission("hg." + cmdName))
-			sender.sendMessage(ChatColor.RED + Config.l_dont_have_permission +": " + ChatColor.GOLD + "/hg " + cmdName);
-		else if (forceInGame && !HG.plugin.players.containsKey(player.getName()))
-			sender.sendMessage(ChatColor.RED + "Your not in a valid game!");
-		else if (forceInRegion && !HG.manager.isInRegion(player.getLocation()))
-			sender.sendMessage(ChatColor.RED + "Your not in a valid HungerGames region!");
-		else if (argLength > arg.length)
-		Util.scm(s, "&4Wrong usage: " + sendHelpLine());
-		else return run();
-		return true;
-	}
+        if (forcePlayer)
+        {
+            if (s instanceof Player)
+            {
+                player = (Player) s;
+            }
+            else
+            {
+                Util.log("You can not use this command in console.");
+                return false;
+            }
+        }
+        if (!s.hasPermission("hg." + cmdName))
+            sender.sendMessage(ChatColor.RED + Config.l_dont_have_permission + ": "
+                    + ChatColor.GOLD + "/hg " + cmdName);
+        else if (forceInGame && !HG.plugin.players.containsKey(player.getName()))
+            sender.sendMessage(ChatColor.RED + "Your not in a valid game!");
+        else if (forceInRegion && !HG.manager.isInRegion(player.getLocation()))
+            sender.sendMessage(ChatColor.RED + "Your not in a valid HungerGames region!");
+        else if (argLength > arg.length)
+            Util.scm(s, "&4Wrong usage: " + sendHelpLine());
+        else
+            return run();
+        return true;
+    }
 
-	public abstract boolean run();
+    public abstract boolean run();
 
-	public String sendHelpLine() {
-		return "&c/&3hg " + cmdName + " &b" + usage;
-	}
+    public String sendHelpLine()
+    {
+        return "&c/&3hg " + cmdName + " &b" + usage;
+    }
 }
