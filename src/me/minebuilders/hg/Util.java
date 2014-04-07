a912cd45b13ce987662c5116e3b3eaab01fc0615
NewibieOrange <yutiancheng1999@gmail.com>
2014-04-07 13:33:28 +0800

    Fixed getTargetBlock doesnt work

--- src/me/minebuilders/hg/Util.java
+++ src/me/minebuilders/hg/Util.java
@@ -18,7 +18,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.material.Attachable;
 import org.bukkit.material.MaterialData;
-import org.bukkit.util.Vector;
+//import org.bukkit.util.Vector;
 
 import me.minebuilders.hg.Config;
 
@@ -157,18 +157,20 @@ public class Util
         return false;
     }
 
+    @SuppressWarnings("deprecation")
     public static Block getTargetBlock(Player player, int range)
     {
-        Location loc = player.getEyeLocation();
-        Vector dir = loc.getDirection().normalize();
+        /*
+         * Location loc = player.getEyeLocation(); Vector dir =
+         * loc.getDirection().normalize();
+         * 
+         * Block b = null;
+         * 
+         * for (int i = 0; i <= range; i++) { b = loc.add(dir).getBlock(); }
+         * 
+         * return b;
+         */
 
-        Block b = null;
-
-        for (int i = 0; i <= range; i++)
-        {
-            b = loc.add(dir).getBlock();
-        }
-
-        return b;
+        return player.getTargetBlock(null, range);
     }
 }
