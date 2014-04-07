949ae042a82c358d55c8c3b2709761c16a3a0830
NewibieOrange <yutiancheng1999@gmail.com>
2014-04-07 15:42:13 +0800

    Clean up

--- src/me/minebuilders/hg/commands/BaseCmd.java
+++ src/me/minebuilders/hg/commands/BaseCmd.java
@@ -41,7 +41,7 @@ public abstract class BaseCmd
                     + ChatColor.GOLD + "/hg " + cmdName);
         else if (forceInGame && !HG.plugin.players.containsKey(player.getName()))
             sender.sendMessage(ChatColor.RED + "Your not in a valid game!");
-        else if (forceInRegion && !HG.manager.isInRegion(( player.getLocation())))
+        else if (forceInRegion && !HG.manager.isInRegion(player.getLocation()))
             sender.sendMessage(ChatColor.RED + "Your not in a valid HungerGames region!");
         else if (argLength > arg.length)
             Util.scm(s, "&4Wrong usage: " + sendHelpLine());
